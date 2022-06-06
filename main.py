59d965941da3bae1a7e68d98592423870e920671
Elliot <EKnock211@gmail.com>
2022-06-06 13:10:22 +0100

    Fixes.

--- main.py
+++ main.py
@@ -9,13 +9,6 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-
-def get_mouse_click_coordinates(x, y):
-    print(x, y)
-
-
-turtle.onscreenclick(get_mouse_click_coordinates)
-
 all_states = data.state.to_list()
 guessed_states = []
 
